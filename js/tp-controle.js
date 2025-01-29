93989f6cc953599b7c3d6495fc3b8a5b7d767f25
foolhardy45 <ajincatayrell@gmail.com>
2025-01-29 16:36:05 +0100

    fin de l'exo tp-controle.js

--- js/tp-controle.js
+++ js/tp-controle.js
@@ -10,6 +10,16 @@ module.exports = {
   Renvoyer le tableau final.
 */
   F1() {
-
+    let newArray = [];
+    for (let i = 0; i <= 35; i++) {
+      if(i%3 === 0 && i %5 === 0) {
+        newArray.push("Python");
+      }else if(i%3 === 0){
+        newArray.push("JavaScript");
+      }else if(i%5 === 0){
+        newArray.push("TypeScript");
+      }
+    }
+    return newArray;
   },
 };
